d3cdcb57cc7589958aec773f6c43125d5e1ffcce
Davi Cunha <davi.pinheiro-costa-cunha@insa-lyon.fr>
2024-09-08 00:49:25 -0300

    feat: @RequireChannelPermission
    @RequirePermission decorator split into @RequireMemberPermission, to check for global guild member's permission and @RequireChannelPermission, to check for a member's permission for a channel

--- src/utils/decorators/RequirePermission.ts
+++ src/utils/decorators/RequirePermission.ts
@@ -1,10 +1,11 @@
-import { PermissionResolvable } from "discord.js";
+import { BaseInteraction, ButtonInteraction, CommandInteraction, GuildMember, ModalSubmitInteraction, PermissionResolvable, StringSelectMenuInteraction } from "discord.js";
+import { CustomInteractionType } from "../../models/CustomInteraction";
 
 /**
  * Decorating an interaction class with this decorator will make it so that it will only execute if the user has all of the specified permissions.
  * @param permissions The permissions to check for.
  */
-export function RequirePermission(...permissions: PermissionResolvable[]) {
+export function RequireMemberPermission(...permissions: PermissionResolvable[]) {
     return function (constructor: any) {
         const originalExecute = constructor.prototype.execute;
 
@@ -13,15 +14,39 @@ export function RequirePermission(...permissions: PermissionResolvable[]) {
         }
 
         constructor.prototype.execute = async function (...args: any[]) {
-            const interaction = args[0];
+            const interaction: CustomInteractionType = args[0];
             if (!interaction?.member?.permissions) return;
 
-            const hasPermission = permissions.every(permission => interaction.member.permissions.has(permission));
+            const hasPermission = (interaction.member as GuildMember).permissions.has(permissions);
 
             if (!hasPermission) {
-                if (interaction.replied || interaction.deferred) return;
+                await interaction.replyOrFollowUp({ content: 'Você não tem permissão para fazer isso.', ephemeral: true });
+                return;
+            }
 
-                await interaction.reply({ content: 'Você não tem permissão para fazer isso.', ephemeral: true });
+            return originalExecute.apply(this, args);
+        };
+    };
+}
+
+export function RequireChannelPermission(...permissions: PermissionResolvable[]) {
+    return function (constructor: any) {
+        const originalExecute = constructor.prototype.execute;
+
+        if (typeof originalExecute !== 'function') {
+            throw new Error(`RequirePermission decorator: The class ${constructor.name} does not have an 'execute' method.`);
+        }
+
+        constructor.prototype.execute = async function (...args: any[]) {
+            const interaction: CustomInteractionType = args[0];
+            if (!interaction?.member?.permissions) return;
+
+            if (!interaction.channel || interaction.channel.isDMBased()) return;
+
+            const hasPermission = interaction.channel.permissionsFor(interaction.member as GuildMember).has(permissions);
+
+            if (!hasPermission) {
+                await interaction.replyOrFollowUp({ content: 'Você não tem permissão para fazer isso.', ephemeral: true });
                 return;
             }
 
